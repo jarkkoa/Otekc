6dad0a55c68aed467e2431808ab14c183e4d4b29
Jarkko Ahvenniemi <Jarge98@gmail.com>
2022-02-18 05:10:37 +0200

    Add function pointer exercise

---
+++ osa2/muunna/muunna.c
@@ -0,0 +1,12 @@
+#include "muunna.h"
+
+
+void muunnaTaulu(unsigned int koko, double taulu[], double (*muunnos)(double))
+{
+    int i;
+
+    for (i = 0; i < koko; i++)
+    {
+        taulu[i] = muunnos(taulu[i]);
+    }
+}
